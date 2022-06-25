2a8d015a6751757eb83c48b9a7e755d7342fc202
Gene Nakagaki <genenakagaki@Genes-MacBook-Air.local>
2022-06-26 00:17:19 +0900

    feat: add util functions

--- settings/gn-setup-elisp.el
+++ settings/gn-setup-elisp.el
@@ -4,4 +4,15 @@
 (general-def '(visual)  emacs-lisp-mode-map
   "<s-return>" 'eval-last-sexp)
 
+(defun gn/read-file (file)
+  "File to string function"
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+(defun gn/print-all ()
+  (interactive)
+  (setq eval-expression-print-length 1000)
+  (setq eval-expression-print-level 1000))
+
 (provide 'gn-setup-elisp)
